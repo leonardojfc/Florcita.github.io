ee11b9bd0d44e50f447140d8c8a5636ef34f7328
leonardojfc <leonardojfc9@gmail.com>
2023-09-21 23:45:54 -0500

    Florcita para ti

--- public/scripts.js
+++ public/scripts.js
@@ -4,9 +4,3 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
-
-// JavaScript para mostrar gradualmente el nombre
-const nombreElement = document.getElementById('nombre');
-setTimeout(() => {
-    nombreElement.style.opacity = '1';
-}, 3000); // 3000ms (3 segundos) coincide con la duración de la animación
